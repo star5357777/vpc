ff5189a8a9f77cf627f15387b6f3affed847ee05
star5357777 <108916391+star5357777@users.noreply.github.com>
2023-03-16 18:30:28 +0900

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,16 @@
+variable "access_key" {
+  type = string
+}
+
+variable "secret_key" {
+  type = string
+}
+
+#-------- vpc
+variable "vpc_cidr" {
+  type = string
+}
+
+variable "vpc_name" {
+  type = string
+}
